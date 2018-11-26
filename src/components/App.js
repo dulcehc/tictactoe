8bb0e415c2c9fb1b1ec531ad3cccf8eaee341caf
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-25 18:48:54 -0600

    Add Board component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Board from './Board';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xPlayer: true
+    }
+  }
+
+  handleClick = (i) => {
+    console.log('handle click: ', i);
+  }
+
   render() {
+    console.log('array squares: ', this.state.squares);
     return (
       <div className="App">
         <h1>Tic Tac Toe</h1>
+        <Board
+          squares={this.state.squares}
+          onClick={(i) => this.handleClick(i)}
+          />
       </div>
     );
   }
